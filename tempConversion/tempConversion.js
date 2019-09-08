0be208416b092b911d6bd6f749f7dd90e45f29f8
Michał <michalbkl@poczta.fm>
2019-09-08 22:53:57 +0200

    solution

--- tempConversion/tempConversion.js
+++ tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(temp) {
+  return Math.round(((5/9) * (temp-32)) * 10) / 10
 }
 
-const ctof = function() {
-
+const ctof = function(temp) {
+  return Math.round((((9/5) * temp + 32)) * 10) / 10
 }
 
 module.exports = {
