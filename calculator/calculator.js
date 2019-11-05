80c56f3b3baf5c6d2436fcb84d960c6946955d78
Michał <michalbkl@poczta.fm>
2019-11-05 20:24:39 +0100

    solution

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,25 +1,44 @@
 function add () {
-	
+	return arguments[0] + arguments[1];	
 }
 
 function subtract () {
-	
+	return arguments[0] - arguments[1];
 }
 
 function sum () {
-	
+	let array = arguments[0];
+	let suma = 0;
+
+	for (let i = 0; i < array.length; i++) {
+		suma += array[i];
+	}
+
+	return suma;
 }
 
 function multiply () {
-	
+	let array = arguments[0];
+	let suma = array[0];
+
+	for (let i = 1; i < array.length; i++) {
+		suma *= array[i];
+	}
+
+	return suma;
 }
 
 function power() {
-	
+	return Math.pow(arguments[0], arguments[1]);
 }
 
-function factorial() {
-	
+function factorial(num) {
+	if (num == 0 || num == 1) {
+		return 1;
+	}
+	else {
+		return num * factorial(num-1);
+	}
 }
 
 module.exports = {
